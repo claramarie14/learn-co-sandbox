a11b2d9c6d86d50b6ff98eb94fc1f62a1da3881e
claramarie14 <shs.claracabrera@gmail.com>
2018-08-10 20:08:47 +0000

    automatically backed up by learn

--- tictactoe.js
+++ tictactoe.js
@@ -8,6 +8,18 @@ $(document).ready(function(){
  
  function markCell() {
    
+   for (var i=MARK;  i <= 100; i++){
+  if (i%3==0 && i%5==0){
+    console.log("fizzbuzz")}
+  else if (i%5==0){
+    console.log ("buzz")}
+  else if (i%3==0){ 
+    console.log ("fizz")}
+  else 
+    console.log (i)
+  }
+
+   
    
    this.innerText = MARK
  }   
