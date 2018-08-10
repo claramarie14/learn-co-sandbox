e80b36d949e78479aa1e54999428d4287f4ea103
claramarie14 <shs.claracabrera@gmail.com>
2018-08-10 19:58:09 +0000

    automatically backed up by learn

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  
-  
-
-  
+  $(".cell").on("click", function() {
+      alert("I was clicked!")
+      //comment out this alert and put your more exciting code here! 
+    });
 })
