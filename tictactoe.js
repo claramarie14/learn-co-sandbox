6cf55396693de5f91bba1c0b0f607766fce6be3e
claramarie14 <shs.claracabrera@gmail.com>
2018-08-10 20:13:39 +0000

    automatically backed up by learn

--- tictactoe.js
+++ tictactoe.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
    
    for (var i=0;  i <= 100; i++){
       if (i%2===0){
-    console.log ("MARK")}
+    console.log (MARK)}
       else {
     console.log ("o")}
   }
