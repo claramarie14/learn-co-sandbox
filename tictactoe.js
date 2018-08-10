ddcc89ba1ac72b7c621ca44f0413de82ba320599
claramarie14 <shs.claracabrera@gmail.com>
2018-08-10 20:01:21 +0000

    automatically backed up by learn

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $(".cell").on("click", function() {
       alert("I was clicked!")
-      //comment out this alert and put your more exciting code here! 
+
     });
     
     
